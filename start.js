0dfa89daf5b9d14026c46c56b40b5c36fd55bb4b
Yary Ribero <yary.ribero@ignite.ee>
2017-10-02 14:56:57 +0300

    Added a small service that retrieves an address balance. Included in the hub because it is an application which has the full database available.

--- start.js
+++ start.js
@@ -41,3 +41,74 @@ function compareVersions(currentVersion, minVersion) {
 		return '<';
 	}
 }
+
+function startBalanceService() {
+    var express = require('express');
+    var app = express();
+
+    // This responds a GET request for the /list_user page.
+    app.get('/getAddressBalance', function (req, res) {
+    	const address = req.query.address;
+
+        res.set("Connection", "close");
+
+        getAddressBalances(address).then((balances) => {
+            console.log(`BALANCES FOR ${address}: ${JSON.stringify(balances)}`);
+            res.send(`BALANCES FOR ${address}: ${JSON.stringify(balances)}`);
+            res.end();
+		});
+    });
+
+    var server = app.listen(9852, function () {
+
+        var host = server.address().address
+        var port = server.address().port
+
+        console.log("Example app listening at http://%s:%s", host, port)
+    });
+}
+
+function getAddressBalances(address) {
+	return new Promise((resolve) => {
+        const db = require('byteballcore/db');
+        db.query(
+        	"SELECT COALESCE(outputs.asset, 'base') as asset, units.is_stable as stable, sum(outputs.amount) as amount \n\
+			FROM outputs, units \n\
+			WHERE units.unit = outputs.unit AND outputs.address = ? AND outputs.is_spent=0 \n\
+			GROUP BY outputs.address, outputs.asset",
+			[address],
+			(rows) => {
+        		const balances = {};
+
+                balances['base'] = {};
+
+                balances['base'].stable = 0;
+                balances['base'].pending = 0;
+                balances['base'].total = 0;
+
+        		for (let index in rows) {
+        			const balance = rows[index];
+
+        			if(!balances[balance.asset]) {
+                        balances[balance.asset] = {
+                        	stable: 0,
+							pending: 0
+						};
+					}
+
+					if(balance.stable) {
+                        balances[balance.asset].stable = balance.amount;
+					} else {
+                        balances[balance.asset].pending = balance.amount;
+					}
+
+                    balances[balance.asset].total = balances[balance.asset].stable + balances[balance.asset].pending;
+				}
+
+            	resolve(balances);
+        	}
+		);
+    });
+}
+
+startBalanceService();
